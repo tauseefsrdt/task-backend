44b3bf875505f3baee5d250b38657b47dfa133a4
Tauseef Alam <tauseef.srdt@gmail.com>
2026-02-04 15:41:48 +0530

    cors error fixed

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ connectDB();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174", process.env.FRONTEND_URL].filter(Boolean),
+    origin: ["https://task-frontend-six-gamma.vercel.app", process.env.FRONTEND_URL].filter(Boolean),
     credentials: true,
-  })
+  }),
 );
 
 app.use(morgan("dev"));
